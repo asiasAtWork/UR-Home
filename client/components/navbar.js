0d23a9cbec55c93119869e2ff8009f8439428a6c
asiasAtWork <asiasatwork@gmail.com>
2023-08-15 23:59:20 -0500

    Initial navbar

---
+++ client/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/moon-magic-logo.png';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={logo} alt="Moon Magic Logo"/>
+            <div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
