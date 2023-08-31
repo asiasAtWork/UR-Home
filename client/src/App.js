87814dbb891eba1fe0b164843fda269c664eff78
asiasAtWork <asiasatwork@gmail.com>
2023-08-30 23:59:29 -0500

    Add footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'; 
 import Navbar from "./components/navbar";
+import Footer from "./components/footer"
 import Landing from './pages/landing';
 import Products from './pages/products';
 import AboutUs from './pages/aboutus';
@@ -15,6 +16,7 @@ function App() {
         </Routes>
       </Router>
       <Landing />
+      <Footer />
     </div>
   );
 }
--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faInstagram, faYelp, faGoogle } from '@fortawesome/free-brands-svg-icons'
+import footerStyles from '../css_modules/Footer.module.css';
+import { ReactComponent as Logo } from '../assets/moon-magic-logo.svg';
 
 const Footer = () => {
     return (
-        <footer>
-            
+        <footer className={`${footerStyles.footer}`}>
+            <hr className={`${footerStyles.footerdivider}`}/>
+            <Logo className={`${footerStyles.footerlogo}`}/>
+            <div className={`${footerStyles.footercontent}`}>
+                <p>Connect with Moon Magic Florist</p>
+                <div className={`${footerStyles.footericons}`}>
+                    <FontAwesomeIcon icon={faFacebook} size="2xl" />
+                    <FontAwesomeIcon icon={faTwitter} size="2xl" />
+                    <FontAwesomeIcon icon={faInstagram} size="2xl" />
+                    <FontAwesomeIcon icon={faYelp} size="2xl"/>
+                    <FontAwesomeIcon icon={faGoogle} size="2xl"/>
+                </div>
+                <div className={`${footerStyles.footerlinks}`}>
+                    <div className={`${footerStyles.footerlinksection}`}>
+                        <p className={`font-light`}>Account Sign Up | Login</p>
+                        <p className={`font-light`}>Terms of Service</p>
+                        <p className={`font-light`}>Refund Policy</p>
+                    </div>
+                    <div className={`${footerStyles.footerlinksection}`}>
+                        <p className={`font-light`}>Site Map</p>
+                        <p className={`font-light`}>Careers</p>
+                        <p className={`font-light`}>FAQs</p>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
